57d637fc225de973fd3903037b0841c77b6984c7
Juan Tenezaca <tenj34@gmail.com>
2018-09-23 17:21:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,13 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name,grade,id = some_id)
-    @id = some_id
+  def initialize(name,grade,id = 1)
+    @id = id
     @name = name
     @grade = grade
-
   end
+
+  def self.create_table
+
+
 end
